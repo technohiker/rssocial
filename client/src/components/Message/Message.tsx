06c9be9a7fdfaa4241ce7de9e51dc24a229b9587
FEZ <75589254+fez-github@users.noreply.github.com>
2023-03-19 14:42:07 +0000

    Changes to Message object.

--- client/src/components/Message/Message.tsx
+++ client/src/components/Message/Message.tsx
@@ -1,10 +1,28 @@
-import { IRSSItem } from "../../types/IRSS";
+import { IMessage } from "../../types/IMessage";
 import "./Message.css";
+import { Button, Card, CardBody, CardFooter, CardTitle } from "reactstrap";
 
 export function Message({ message }: IMessageProps) {
-  return <p>{message.description}</p>;
+
+  //Have function that, when user clicks link, sends backend request to mark message as read.
+  return (<Card>
+    <CardTitle>
+      <h1><a href={message.source_link}>{message.title}</a></h1>
+      <h6>{message.date_created}</h6>
+      <h6>{message.author}</h6>
+      {/** Indicator that message was read. */}
+    </CardTitle>
+    <CardBody>
+      <h4>{message.description}</h4>
+      <h5>{message.content}</h5>
+    </CardBody>
+    <CardFooter>
+      <Button name="emoteButton"></Button>
+      <Button name="folderButton"></Button>
+    </CardFooter>
+  </Card>)
 }
 
 interface IMessageProps {
-  message: IRSSItem;
+  message: IMessage;
 }
--- client/src/components/MessageList/MessageList.tsx
+++ client/src/components/MessageList/MessageList.tsx
@@ -1,5 +1,14 @@
+import { IMessage } from '../../types/IMessage'
+import { Message } from '../Message/Message'
 import './MessageList.css'
 
-export function MessageList(){
-    return(<p>Placeholder.</p>)
+/** List of messages.(what's the best way to present these?  avoid infinite scroll) */
+export function MessageList({messages}: IMessageListProps){
+    return(messages.map(msg => {
+        <Message key={msg.messageID} message={msg} />
+    }))
+}
+
+interface IMessageListProps{
+    messages: IMessage[]
 }
