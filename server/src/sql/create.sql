262f81be11dc458b4c01654e372dd7d00b6a22af
Mark <hansen-mark@hotmail.com>
2023-05-18 16:46:41 -0400

    Updated setup SQL file.

--- server/src/sql/create.sql
+++ server/src/sql/create.sql
@@ -1,10 +1,10 @@
 \echo 'Delete and recreate capstone_2 db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE capstone_2;
-CREATE DATABASE capstone_2
-  WITH ENCODING = 'UTF8';
-\connect capstone_2
+-- DROP DATABASE deqbl8450smu2e;
+-- CREATE DATABASE deqbl8450smu2e
+ -- WITH ENCODING = 'UTF8';
+\connect deqbl8450smu2e
 
 \i src/sql/schema.sql
 \i src/sql/seed.sql
