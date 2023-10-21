f7159c0826912c289e15c0f738862c55260a9a8d
Mark <hansen-mark@hotmail.com>
2023-10-21 19:06:07 -0400

    Fixed issue with sending emails.

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -4,71 +4,72 @@ import { Form } from "react-final-form";
 import { FieldInput } from "../../helpers/FormFields/FieldInput";
 import { Card, CardBody, CardTitle, Button } from "reactstrap";
 import { LinkButton } from "../LinkButton/LinkButton";
+import "./LoginForm.css";
 
 /** Form for logging the user in. */
 export function LoginForm({ onSubmission }: ILoginFormProps) {
-  const required = (value: any) => (value ? undefined : "Required");
-  const [error, setError] = useState("");
-  const history = useHistory();
+	const required = (value: any) => (value ? undefined : "Required");
+	const [error, setError] = useState("");
+	const history = useHistory();
 
-  const submission = async (evt: ILoginFormSubmit) => {
-    let result = await onSubmission(evt.username, evt.password);
-    if (result) {
-      setError(result[0]);
-    } else {
-      history.push("/");
-    }
-  };
+	const submission = async (evt: ILoginFormSubmit) => {
+		let result = await onSubmission(evt.username, evt.password);
+		if (result) {
+			setError(result[0]);
+		} else {
+			history.push("/");
+		}
+	};
 
-  return (
-    <Card className="card-form">
-      <CardTitle className="text-center">
-        Please submit your username and password.
-      </CardTitle>
-      <CardBody>
-        <Form
-          onSubmit={submission}
-          render={({ handleSubmit, submitting }) => (
-            <form onSubmit={handleSubmit}>
-              <p className="text-center">{error}</p>
-              <FieldInput
-                name="username"
-                className="mb-3"
-                validation={required}
-                label={"Username:"}
-                type={"text"}
-                placeholder={""}
-              />
-              <FieldInput
-                name="password"
-                className="mb-3"
-                validation={required}
-                label={"Password:"}
-                type={"password"}
-                placeholder={""}
-              />
-              <div className="d-flex justify-content-evenly">
-                <Button type="submit" disabled={submitting}>
-                  Submit
-                </Button>
-                <LinkButton link="/" className="btn-danger" text="Cancel" />
-              </div>
-            </form>
-          )}
-        />
-      </CardBody>
-    </Card>
-  );
+	return (
+		<Card className="card-form">
+			<CardTitle className="text-center">
+				Please submit your username and password.
+			</CardTitle>
+			<CardBody>
+				<Form
+					onSubmit={submission}
+					render={({ handleSubmit, submitting }) => (
+						<form onSubmit={handleSubmit}>
+							<p className="text-center">{error}</p>
+							<FieldInput
+								name="username"
+								className="mb-3"
+								validation={required}
+								label={"Username:"}
+								type={"text"}
+								placeholder={""}
+							/>
+							<FieldInput
+								name="password"
+								className="mb-3"
+								validation={required}
+								label={"Password:"}
+								type={"password"}
+								placeholder={""}
+							/>
+							<div className="d-flex justify-content-evenly">
+								<Button type="submit" disabled={submitting}>
+									Submit
+								</Button>
+								<LinkButton link="/" className="btn-danger" text="Cancel" />
+							</div>
+						</form>
+					)}
+				/>
+			</CardBody>
+		</Card>
+	);
 }
 
 interface ILoginFormSubmit {
-  username: string;
-  password: string;
+	username: string;
+	password: string;
 }
 
 interface ILoginFormProps {
-  onSubmission: (
-    username: string,
-    password: string
-  ) => Promise<undefined | string[]>;
+	onSubmission: (
+		username: string,
+		password: string
+	) => Promise<undefined | string[]>;
 }
